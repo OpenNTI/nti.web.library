49e8043ff9e500e43c5b8c0d53622e0be98cac5b
Landon Sherwood <landonsherwood23@gmail.com>
2018-08-29 14:48:18 -0500

    Move book card to content repo

--- src/components/items/Bundle.jsx
+++ src/components/items/Bundle.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import defaultImage from '../../resources/images/default-card-image.png';
+import {BookCard} from '@nti/web-content';
 
 export default class Bundle extends React.Component {
 	static propTypes = {
@@ -15,18 +14,9 @@ export default class Bundle extends React.Component {
 
 	render () {
 		const {props: {item}} = this;
-		let {byline, author, title} = item || {};
 
 		return (
-			<div className="book-card">
-				<div className="bundle-card-image">
-					<img src={defaultImage} />
-				</div>
-				<label>
-					<h3 className="book-title">{title}</h3>
-					<address className="book-author">By {byline || author}</address>
-				</label>
-			</div>
+			<BookCard bundle={item} />
 		);
 	}
 }
