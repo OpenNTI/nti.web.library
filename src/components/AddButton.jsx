53b9e5cc57bacd55752cfe1ad32396a05ef65df7
Landon Sherwood <landonsherwood23@gmail.com>
2018-09-12 11:04:53 -0500

    NTI-6602 fix add button

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -4,12 +4,15 @@ import {LinkTo} from '@nti/web-routing';
 
 export default class AddButton extends React.Component {
 	static contextTypes = {
+		router: PropTypes.object,
 		basePath: PropTypes.string
 	}
 
 	render () {
+		const baseroute = this.context.basePath != null ? this.context.basePath : this.context.router.baseroute.replace('library', '');
+
 		return (
-			<LinkTo.Path to={this.context.basePath + '/catalog'} className="button library-add">
+			<LinkTo.Path to={baseroute + '/catalog'} className="button library-add">
 				Add
 			</LinkTo.Path>
 		);
