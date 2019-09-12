c7b533172a395f025c5a46e2edfe6863f41a1e1e
Andrew Ligon <andrew.ligon@nextthought.com>
2019-09-12 13:45:16 -0500

    use the community card from community

--- src/components/items/Community.jsx
+++ src/components/items/Community.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {User} from '@nti/web-profiles';
-
-const {CommunityCard} = User;
+import {Community} from '@nti/web-profiles';
 
 export default class CommunityItem extends React.Component  {
 	static handles (item) {
@@ -17,7 +15,7 @@ export default class CommunityItem extends React.Component  {
 		let {item} = this.props;
 
 		return (
-			<CommunityCard community={item} />
+			<Community.Card community={item} />
 		);
 	}
 }
