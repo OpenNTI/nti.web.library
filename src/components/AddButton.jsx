c8eac125ee8c4beb3023dc3c7b9c94464189497f
Jonathan Grimes <jsg2021@gmail.com>
2020-11-18 14:30:03 -0600

    cleanup the AddButton, ensure route is /base/catalog, and not /base//catalog

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,8 +1,20 @@
-import './AddButton.scss';
+import {join} from 'path';
+
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import {scoped} from '@nti/lib-locale';
 import {LinkTo} from '@nti/web-routing';
+
+const styles = css`
+.add-button {
+	color: var(--primary-blue);
+	cursor: pointer;
+	font: normal 600 0.875em/2em var(--body-font-family);
+	font-size: 14px;
+	text-decoration: none;
+}
+`;
 
 const t = scoped('library.components.AddButton', {
 	courses: '+ Add'
@@ -21,12 +33,12 @@ export default class AddButton extends React.Component {
 
 	render () {
 		const {section} = this.props;
-		const baseroute = this.context.basePath != null ? this.context.basePath : this.context.router.baseroute.replace('library', '');
+		const baseroute = this.context.basePath ?? this.context.router.baseroute.replace('library', '');
 
 		if (t.isMissing(section)) { return null; }
 
 		return (
-			<LinkTo.Path to={baseroute + '/catalog'} className="button library-add">
+			<LinkTo.Path to={join(baseroute, 'catalog')} className={cx('button', 'library-add', styles.addButton)}>
 				{t(section)}
 			</LinkTo.Path>
 		);
