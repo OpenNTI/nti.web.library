8749e35cc0bfa939974e297d1ea991c326640c59
Ray Hatfield <ray.hatfield@gmail.com>
2021-02-25 09:32:38 -0600

    introduce adaptive/centered grid layout for course cards

--- src/components/containers/Collection.jsx
+++ src/components/containers/Collection.jsx
@@ -5,52 +5,45 @@ import cx from 'classnames';
 
 import getItem from '../items';
 
+import CenteredGrid from './CenteredGrid';
+
 const isEmpty = s => s == null || s === '';
 
-export default class LibraryCollection extends React.Component {
-	static propTypes = {
-		className: PropTypes.string,
-		list: PropTypes.array,
-		title: PropTypes.string,
-		subtitle: PropTypes.string,
-		onModification: PropTypes.func,
-		children: PropTypes.any,
-	};
+const Title = ({title, subtitle}) => isEmpty(title) ? null : (
+	<h5>
+		{title}
+		<label>{subtitle}</label>
+	</h5>
+);
 
-	render() {
-		const {
-			props: {
-				children,
-				className,
-				list,
-				title,
-				subtitle,
-				onModification,
-			},
-		} = this;
+const Container = props => <CenteredGrid colsize={242} gap={14} {...props} />
 
-		const titleRow = isEmpty(title) ? null : (
-			<h5>
-				{title}
-				<label>{subtitle}</label>
-			</h5>
-		);
-
-		return (
-			<div className={cx('library-collection', className)}>
-				{titleRow}
+export default function LibraryCollection ({
+	children,
+	className,
+	list,
+	title,
+	subtitle,
+	onModification,
+}) {
+	return (
+		<Container className={cx('library-collection', className)}>
+			<CenteredGrid.Header>
+				<Title title={title} subtitle={subtitle} />
 				{children}
-				<ul>
-					{list.map(item => {
-						const Item = getItem(item);
-						const key =
-							item.NTIID ||
-							item.href ||
-							item?.CatalogEntry?.NTIID ||
-							item?.CatalogEntry?.href;
+			</CenteredGrid.Header>
+			<Container as="ul">
+				{list.map(item => {
+					const Item = getItem(item);
+					const key =
+						item.NTIID ||
+						item.href ||
+						item?.CatalogEntry?.NTIID ||
+						item?.CatalogEntry?.href;
 
-						return (
-							Item && (
+					return (
+						Item && (
+							<>
 								<li
 									className="library-object"
 									data-testid={key}
@@ -61,11 +54,61 @@ export default class LibraryCollection extends React.Component {
 										onModification={onModification}
 									/>
 								</li>
-							)
-						);
-					})}
-				</ul>
-			</div>
-		);
-	}
+								{/* repeated for dev purposes. take this out. */}
+								<li
+									className="library-object"
+									data-testid={key}
+									key={key + Math.random()}
+								>
+									<Item
+										item={item}
+										onModification={onModification}
+									/>
+								</li>
+								<li
+									className="library-object"
+									data-testid={key}
+									key={key + Math.random()}
+								>
+									<Item
+										item={item}
+										onModification={onModification}
+									/>
+								</li>
+								<li
+									className="library-object"
+									data-testid={key}
+									key={key + Math.random()}
+								>
+									<Item
+										item={item}
+										onModification={onModification}
+									/>
+								</li>
+								<li
+									className="library-object"
+									data-testid={key}
+									key={key + Math.random()}
+								>
+									<Item
+										item={item}
+										onModification={onModification}
+									/>
+								</li>
+							</>
+						)
+					);
+				})}
+			</Container>
+		</Container>
+	);
 }
+
+LibraryCollection.propTypes = {
+	className: PropTypes.string,
+	list: PropTypes.array,
+	title: PropTypes.string,
+	subtitle: PropTypes.string,
+	onModification: PropTypes.func,
+	children: PropTypes.any,
+};
