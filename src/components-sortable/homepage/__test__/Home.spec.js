33336d7e1bf44310f934e444b2f32874b85c634a
Ray Hatfield <ray.hatfield@gmail.com>
2021-04-15 12:18:49 -0500

    pick up renames in corresponding tests

--- src/components-sortable/homepage/__test__/Home.spec.js
+++ src/components-sortable/homepage/__test__/Home.spec.js
@@ -7,7 +7,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { render } from '@testing-library/react';
 
-import HomePage from '../View';
+import { Home as HomePage } from '../Home';
 
 const onBefore = () => {
 	global.$AppConfig = {
