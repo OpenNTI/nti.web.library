8f934cc87d230d2a3c5263a24fb89e5570d74c5e
Ray Hatfield <ray.hatfield@gmail.com>
2021-05-03 16:37:07 -0500

    test data cleanup

--- src/components-sortable/homepage/__test__/library-data.js
+++ src/components-sortable/homepage/__test__/library-data.js
@@ -1,3 +1,23 @@
+const n = x =>
+	`tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:${x}`;
+
+const mockCourse = ({ NTIID, ...props }) => ({
+	title: 'Test Course',
+	isCourse: true,
+	NTIID: n(NTIID),
+	MimeType: 'application/vnd.nextthought.courseware.courseinstanceenrollment',
+	getStartDate: () => {},
+	getEndDate: () => {},
+	CatalogEntry: {
+		CourseNTIID: n(NTIID),
+		getStartDate: () => {},
+		getEndDate: () => {},
+		getAuthorLine: () => {},
+		getDefaultAssetRoot: () => {},
+	},
+	...props,
+});
+
 export default {
 	communities: [
 		{
@@ -14,226 +34,58 @@ export default {
 		Items: [
 			{
 				title: 'Test Course 1',
-				isCourse: true,
-				NTIID:
-					'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P7',
-				MimeType:
-					'application/vnd.nextthought.courseware.courseinstanceenrollment',
-				getStartDate: () => {},
-				getEndDate: () => {},
-				CatalogEntry: {
-					CourseNTIID:
-						'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P7',
-					getStartDate: () => {},
-					getEndDate: () => {},
-					getAuthorLine: () => {},
-					getDefaultAssetRoot: () => {},
-				},
+				NTIID: 'bWj9Mk6A5P7',
 			},
 			{
 				title: 'Test Course 2',
-				isCourse: true,
-				NTIID:
-					'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P8',
-				MimeType:
-					'application/vnd.nextthought.courseware.courseinstanceenrollment',
-				getStartDate: () => {},
-				getEndDate: () => {},
-				CatalogEntry: {
-					CourseNTIID:
-						'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P8',
-					getStartDate: () => {},
-					getEndDate: () => {},
-					getAuthorLine: () => {},
-					getDefaultAssetRoot: () => {},
-				},
+				NTIID: 'bWj9Mk6A5P8',
 			},
 			{
 				title: 'Test Course 3',
-				isCourse: true,
-				NTIID:
-					'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P9',
-				MimeType:
-					'application/vnd.nextthought.courseware.courseinstanceenrollment',
-				getStartDate: () => {},
-				getEndDate: () => {},
-				CatalogEntry: {
-					CourseNTIID:
-						'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P9',
-					getStartDate: () => {},
-					getEndDate: () => {},
-					getAuthorLine: () => {},
-					getDefaultAssetRoot: () => {},
-				},
+				NTIID: 'bWj9Mk6A5P9',
 			},
 			{
 				title: 'Test Course 4',
-				isCourse: true,
-				NTIID:
-					'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P6',
-				MimeType:
-					'application/vnd.nextthought.courseware.courseinstanceenrollment',
-				getStartDate: () => {},
-				getEndDate: () => {},
-				CatalogEntry: {
-					CourseNTIID:
-						'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P6',
-					getStartDate: () => {},
-					getEndDate: () => {},
-					getAuthorLine: () => {},
-					getDefaultAssetRoot: () => {},
-				},
+				NTIID: 'bWj9Mk6A5P6',
 			},
 			{
 				title: 'Test Course 5',
-				isCourse: true,
-				NTIID:
-					'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P5',
-				MimeType:
-					'application/vnd.nextthought.courseware.courseinstanceenrollment',
-				getStartDate: () => {},
-				getEndDate: () => {},
-				CatalogEntry: {
-					CourseNTIID:
-						'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P5',
-					getStartDate: () => {},
-					getEndDate: () => {},
-					getAuthorLine: () => {},
-					getDefaultAssetRoot: () => {},
-				},
+				NTIID: 'bWj9Mk6A5P5',
 			},
 			{
 				title: 'Test Course 6',
-				isCourse: true,
-				NTIID:
-					'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P4',
-				MimeType:
-					'application/vnd.nextthought.courseware.courseinstanceenrollment',
-				getStartDate: () => {},
-				getEndDate: () => {},
-				CatalogEntry: {
-					CourseNTIID:
-						'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P4',
-					getStartDate: () => {},
-					getEndDate: () => {},
-					getAuthorLine: () => {},
-					getDefaultAssetRoot: () => {},
-				},
+				NTIID: 'bWj9Mk6A5P4',
 			},
-		],
+		].map(mockCourse),
 	},
 
 	AdministeredCourses: {
 		Items: [
 			{
 				title: 'Test Course 1',
-				isCourse: true,
-				NTIID:
-					'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P7',
-				MimeType:
-					'application/vnd.nextthought.courseware.courseinstanceenrollment',
-				getStartDate: () => {},
-				getEndDate: () => {},
-				CatalogEntry: {
-					CourseNTIID:
-						'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P7',
-					getStartDate: () => {},
-					getEndDate: () => {},
-					getAuthorLine: () => {},
-					getDefaultAssetRoot: () => {},
-				},
+				NTIID: 'bWj9Mk6A5P7',
 			},
 			{
 				title: 'Test Course 2',
-				isCourse: true,
-				NTIID:
-					'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P8',
-				MimeType:
-					'application/vnd.nextthought.courseware.courseinstanceenrollment',
-				getStartDate: () => {},
-				getEndDate: () => {},
-				CatalogEntry: {
-					CourseNTIID:
-						'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P8',
-					getStartDate: () => {},
-					getEndDate: () => {},
-					getAuthorLine: () => {},
-					getDefaultAssetRoot: () => {},
-				},
+				NTIID: 'bWj9Mk6A5P8',
 			},
 			{
 				title: 'Test Course 3',
-				isCourse: true,
-				NTIID:
-					'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P9',
-				MimeType:
-					'application/vnd.nextthought.courseware.courseinstanceenrollment',
-				getStartDate: () => {},
-				getEndDate: () => {},
-				CatalogEntry: {
-					CourseNTIID:
-						'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P9',
-					getStartDate: () => {},
-					getEndDate: () => {},
-					getAuthorLine: () => {},
-					getDefaultAssetRoot: () => {},
-				},
+				NTIID: 'bWj9Mk6A5P9',
 			},
 			{
 				title: 'Test Course 4',
-				isCourse: true,
-				NTIID:
-					'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P6',
-				MimeType:
-					'application/vnd.nextthought.courseware.courseinstanceenrollment',
-				getStartDate: () => {},
-				getEndDate: () => {},
-				CatalogEntry: {
-					CourseNTIID:
-						'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P6',
-					getStartDate: () => {},
-					getEndDate: () => {},
-					getAuthorLine: () => {},
-					getDefaultAssetRoot: () => {},
-				},
+				NTIID: 'bWj9Mk6A5P6',
 			},
 			{
 				title: 'Test Course 5',
-				isCourse: true,
-				NTIID:
-					'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P5',
-				MimeType:
-					'application/vnd.nextthought.courseware.courseinstanceenrollment',
-				getStartDate: () => {},
-				getEndDate: () => {},
-				CatalogEntry: {
-					CourseNTIID:
-						'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P5',
-					getStartDate: () => {},
-					getEndDate: () => {},
-					getAuthorLine: () => {},
-					getDefaultAssetRoot: () => {},
-				},
+				NTIID: 'bWj9Mk6A5P5',
 			},
 			{
 				title: 'Test Course 6',
-				isCourse: true,
-				NTIID:
-					'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P4',
-				MimeType:
-					'application/vnd.nextthought.courseware.courseinstanceenrollment',
-				getStartDate: () => {},
-				getEndDate: () => {},
-				CatalogEntry: {
-					CourseNTIID:
-						'tag:nextthought.com,2011-10:landon.sherwood-OID-0x1488cb:5573657273:bWj9Mk6A5P4',
-					getStartDate: () => {},
-					getEndDate: () => {},
-					getAuthorLine: () => {},
-					getDefaultAssetRoot: () => {},
-				},
+				NTIID: 'bWj9Mk6A5P4',
 			},
-		],
+		].map(mockCourse),
 	},
 
 	books: {
