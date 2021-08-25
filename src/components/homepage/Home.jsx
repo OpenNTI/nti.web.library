8e3b1fc27312daf237e020b7c75919c7daa11589
Andrew Ligon <aligon90@gmail.com>
2021-08-25 12:52:56 -0500

    NTI-10852 make sure we aren't setting course_filter

--- src/components/homepage/Home.jsx
+++ src/components/homepage/Home.jsx
@@ -231,12 +231,6 @@ const WithCourses = [KEYS.administeredCourses, KEYS.courses].reduce(
 							sortOn,
 							sortOrder,
 							batchSize: 28,
-							...(!searchTerm // omit course_filter when searching
-								? {
-										course_filter:
-											data?.course_filter || 'incomplete',
-								  }
-								: {}),
 					  };
 				return {
 					collection,
